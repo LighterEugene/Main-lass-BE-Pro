ea4b1f5f6fd392054239ba356a2997ed0534b1e0
Eugene <51925892+LighterEugene@users.noreply.github.com>
2024-09-16 22:03:16 +0200

    Finished

--- Main-lass-BE-Pro-homework4/src/Main.java
+++ Main-lass-BE-Pro-homework4/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,5 +26,40 @@ public class Main {
         }
         endTime = System.currentTimeMillis();
         System.out.println("Classic for с вызовом list.size() на каждой итерации: " + (endTime - startTime) + " ms");
+
+        // 3. Classic for с предварительным определением list.size() в переменную
+        startTime = System.currentTimeMillis();
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            int temp = list.get(i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Classic for с определением list.size() в переменную: " + (endTime - startTime) + " ms");
+
+        // 4. Classic for с предварительным определением list.size() и перебором с конца
+        startTime = System.currentTimeMillis();
+        for (int i = size - 1; i >= 0; i--) {
+            int temp = list.get(i);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Classic for с перебором с конца: " + (endTime - startTime) + " ms");
+
+        // 5. Использование Iterator
+        startTime = System.currentTimeMillis();
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            int temp = iterator.next();
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Iterator: " + (endTime - startTime) + " ms");
+
+        // 6. Использование ListIterator
+        startTime = System.currentTimeMillis();
+        ListIterator<Integer> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            int temp = listIterator.next();
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("ListIterator: " + (endTime - startTime) + " ms");
     }
 }
